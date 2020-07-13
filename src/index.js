08e42e9f09e2b9a052c2aaf48f1a4cecf9f853d5
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-13 12:44:49 +1000

    Created js files for all pages and components, included them in index.js, included scss files in js files, included vars.scss in all scss files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './js/common.js';
 //import './css/main.css';
 import './assets/scss/main.scss'
+
+function requireAll(requireContext) {
+    return requireContext.keys().map(requireContext);
+}
+
+requireAll(require.context('./assets/components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
+requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
