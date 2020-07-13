0284e18e2530c7658ab3f9a67e9764524c4d69c2
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-13 18:05:58 +1000

    Successfully changed the project structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './js/common.js';
 //import './css/main.css';
-import './assets/scss/main.scss'
+import './theme/global.scss'
 
 function requireAll(requireContext) {
     return requireContext.keys().map(requireContext);
 }
 
-requireAll(require.context('./assets/components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
+requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
 requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
