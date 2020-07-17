1a6afafe8332c20f2dcd73260ba2fa4b47820801
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-18 00:39:31 +1000

    Fixed an issue when svg-images were damaged because of file-loader

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -92,7 +92,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/, //removed .svg here so that images are not damaged
                 loader: 'file-loader',
                 options: {
                 	name: '[name].[ext]',
