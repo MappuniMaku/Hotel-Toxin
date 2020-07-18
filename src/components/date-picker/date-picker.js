44f9463a2778f3efff1866f051b6deeb631e37c1
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-18 23:01:09 +1000

    Added inputmask to date-picker

--- src/components/date-picker/date-picker.js
+++ src/components/date-picker/date-picker.js
@@ -1,1 +1,15 @@
 import './date-picker.scss';
+
+//var Inputmask = require('inputmask');
+import Inputmask from "inputmask";
+
+Inputmask.extendAliases({
+    'date-ru': {
+        placeholder: 'ДД.ММ.ГГГГ',
+        regex: "^(0[1-9]|[12][0-9]|3[01])\\.(0[1-9]|1[012])\\.(19|20)[0-9][0-9]$",
+    }
+  });
+
+$(document).ready(function() {
+    Inputmask("date-ru").mask(".date-picker .input-field");
+});
