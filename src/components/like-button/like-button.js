eb4005363ac651d26c7af89644d11ddda0bb4c04
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-31 17:17:49 +1000

    Added rate-button, found a way to add event listeners for multiple elements with same class, reworked like-button

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -4,23 +4,44 @@ $(document).ready(function() {
     $(".like-button__input:checked").parent().addClass("like-button__item-checked");
     $(".like-button__item-checked .like-button__like-icon").html("favorite");
 
-    $(".like-button__item[data-id='item-1'] .like-button__input").click(function(){
-        $(this).parent().toggleClass("like-button__item-checked");
+    // $(".like-button__item[data-id='item-1'] .like-button__input").click(function(){
+    //     $(this).parent().toggleClass("like-button__item-checked");
 
-        $(".like-button__item[data-id='item-1'] .like-button__like-icon").html("favorite_border");
-        $(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__like-icon").html("favorite");
+    //     $(".like-button__item[data-id='item-1'] .like-button__like-icon").html("favorite_border");
+    //     $(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__like-icon").html("favorite");
 
-        $(".like-button__item[data-id='item-1'] .like-button__likes-number").html(+$(".like-button__item[data-id='item-1'] .like-button__likes-number").html() - 1);
-        $(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__likes-number").html(+$(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__likes-number").html() + 2)
-    })
+    //     $(".like-button__item[data-id='item-1'] .like-button__likes-number").html(+$(".like-button__item[data-id='item-1'] .like-button__likes-number").html() - 1);
+    //     $(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__likes-number").html(+$(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__likes-number").html() + 2)
+    // })
 
-    $(".like-button__item[data-id='item-2'] .like-button__input").click(function(){
-        $(this).parent().toggleClass("like-button__item-checked");
+    // $(".like-button__item[data-id='item-2'] .like-button__input").click(function(){
+    //     $(this).parent().toggleClass("like-button__item-checked");
 
-        $(".like-button__item[data-id='item-2'] .like-button__like-icon").html("favorite_border");
-        $(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__like-icon").html("favorite");
+    //     $(".like-button__item[data-id='item-2'] .like-button__like-icon").html("favorite_border");
+    //     $(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__like-icon").html("favorite");
 
-        $(".like-button__item[data-id='item-2'] .like-button__likes-number").html(+$(".like-button__item[data-id='item-2'] .like-button__likes-number").html() - 1);
-        $(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__likes-number").html(+$(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__likes-number").html() + 2)
-    })
+    //     $(".like-button__item[data-id='item-2'] .like-button__likes-number").html(+$(".like-button__item[data-id='item-2'] .like-button__likes-number").html() - 1);
+    //     $(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__likes-number").html(+$(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__likes-number").html() + 2)
+    // })
+
+    $(".like-button__item").each(function(i, elem) {
+        $(elem).children(".like-button__input").on("change", function(){
+            $(elem).toggleClass("like-button__item-checked");
+            $(elem).children(".like-button__like-icon").html("favorite_border");
+
+            $(elem).children(".like-button__likes-number").html(+$(elem).children(".like-button__likes-number").html() - 1);
+
+            if($(elem).hasClass("like-button__item-checked")) {
+                $(elem).children(".like-button__like-icon").html("favorite");
+                $(elem).children(".like-button__likes-number").html(+$(elem).children(".like-button__likes-number").html() + 2)
+            };
+        });
+    });
+
+    // $(".asd").each(function(i, elem) {
+    //     $(elem).click(function() {
+    //         $(elem).html(+$(elem).html() + 1)
+    //     })
+
+    // });
 });
