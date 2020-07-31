ff34de605535a2c6f5fc5329d1db4b66f63c7e90
Grigory Telenok <telenok.grigory@gmail.com>
2020-07-31 15:54:35 +1000

    Reworked like-buttond (now we put id in data-id attribute instead of class

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
     $(".like-button__input:checked").parent().addClass("like-button__item-checked");
     $(".like-button__item-checked .like-button__like-icon").html("favorite");
 
-    $(".like-button__item-1 .like-button__input").click(function(){
+    $(".like-button__item[data-id='item-1'] .like-button__input").click(function(){
         $(this).parent().toggleClass("like-button__item-checked");
 
-        $(".like-button__item-1 .like-button__like-icon").html("favorite_border");
-        $(".like-button__item-1.like-button__item-checked .like-button__like-icon").html("favorite");
+        $(".like-button__item[data-id='item-1'] .like-button__like-icon").html("favorite_border");
+        $(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__like-icon").html("favorite");
 
-        $(".like-button__item-1 .like-button__likes-number").html(+$(".like-button__item-1 .like-button__likes-number").html() - 1);
-        $(".like-button__item-1.like-button__item-checked .like-button__likes-number").html(+$(".like-button__item-1.like-button__item-checked .like-button__likes-number").html() + 2)
+        $(".like-button__item[data-id='item-1'] .like-button__likes-number").html(+$(".like-button__item[data-id='item-1'] .like-button__likes-number").html() - 1);
+        $(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__likes-number").html(+$(".like-button__item[data-id='item-1'].like-button__item-checked .like-button__likes-number").html() + 2)
     })
 
-    $(".like-button__item-2 .like-button__input").click(function(){
+    $(".like-button__item[data-id='item-2'] .like-button__input").click(function(){
         $(this).parent().toggleClass("like-button__item-checked");
 
-        $(".like-button__item-2 .like-button__like-icon").html("favorite_border");
-        $(".like-button__item-2.like-button__item-checked .like-button__like-icon").html("favorite");
+        $(".like-button__item[data-id='item-2'] .like-button__like-icon").html("favorite_border");
+        $(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__like-icon").html("favorite");
 
-        $(".like-button__item-2 .like-button__likes-number").html(+$(".like-button__item-2 .like-button__likes-number").html() - 1);
-        $(".like-button__item-2.like-button__item-checked .like-button__likes-number").html(+$(".like-button__item-2.like-button__item-checked .like-button__likes-number").html() + 2)
+        $(".like-button__item[data-id='item-2'] .like-button__likes-number").html(+$(".like-button__item[data-id='item-2'] .like-button__likes-number").html() - 1);
+        $(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__likes-number").html(+$(".like-button__item[data-id='item-2'].like-button__item-checked .like-button__likes-number").html() + 2)
     })
 });
