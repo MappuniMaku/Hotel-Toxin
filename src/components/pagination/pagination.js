427471d823703dc2b8d71af8d7ddb85ab76f8858
Grigory Telenok <telenok.grigory@gmail.com>
2020-08-02 01:54:02 +1000

    Added submit-button and pagination, imported Simple Pagination plugin for this

---
+++ src/components/pagination/pagination.js
@@ -0,0 +1,60 @@
+import './pagination.scss';
+
+require("../../assets/jquery_plugins/simplePagination/jquery.simplePagination.js");
+
+$(document).ready(function() {
+    let items = $(".pagination__placeholder").data("items");
+    let pages = $(".pagination__placeholder").data("pages");
+    let itemsOnPage = $(".pagination__placeholder").data("itemsonpage");
+    let currentPage = $(".pagination__placeholder").data("currentpage");
+    let paginationDescription = $(".pagination .pagination__description");
+
+    $(".pagination__placeholder").pagination({
+        items: items,
+        itemsOnPage: itemsOnPage,
+        pages: pages,
+        currentPage: currentPage,
+        displayedPages: 3,
+        edges: 1,
+        prevText: "arrow_backward",
+        nextText: "arrow_forward",
+        onPageClick: function(pageNumber, event) {
+            let currentItemRange;
+            if(pageNumber != pages) {
+                currentItemRange = `${(pageNumber - 1) * itemsOnPage + 1} – ${pageNumber * itemsOnPage}`;
+            } else if(pageNumber == pages) {
+                currentItemRange = `${(pageNumber - 1) * itemsOnPage + 1} – ${items}`;
+            }
+
+            let totalItems;
+            if(items > 100) {
+                totalItems = "100+";
+            } else {
+                totalItems = items;
+            };
+
+            let descText = currentItemRange + " из " + totalItems + " вариантов аренды";
+
+            paginationDescription.html(descText);
+        },
+        onInit: function() {
+            let currentItemRange;
+            if(currentPage != pages) {
+                currentItemRange = `${(currentPage - 1) * itemsOnPage + 1} – ${currentPage * itemsOnPage}`;
+            } else if(currentPage == pages) {
+                currentItemRange = `${(currentPage - 1) * itemsOnPage + 1} – ${items}`;
+            }
+
+            let totalItems;
+            if(items > 100) {
+                totalItems = "100+";
+            } else {
+                totalItems = items;
+            };
+
+            let descText = currentItemRange + " из " + totalItems + " вариантов аренды";
+
+            paginationDescription.html(descText);
+        },
+    });
+});
