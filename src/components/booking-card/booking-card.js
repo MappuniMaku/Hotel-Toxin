fab44d72ea548f0256da73dc0aec56e52367ec6a
Grigory Telenok <telenok.grigory@gmail.com>
2020-08-07 00:16:14 +1000

    Finished booking-card layout

--- src/components/booking-card/booking-card.js
+++ src/components/booking-card/booking-card.js
@@ -6,7 +6,15 @@ function priceToString(num) {
     if(num < 1000) {
         return num + "₽";
     } else if(num >=1000 && num < 1000000) {
-        return Math.floor(num/1000) + " " + (num - Math.floor(num/1000) * 1000) + "₽";
+
+        let thousands = Math.floor(num/1000);
+        let residue = num - Math.floor(num/1000) * 1000;
+
+        if(residue < 100) {
+            residue = "0" + residue;
+        };
+
+        return thousands + " " + residue + "₽";
     };
 };
 
@@ -19,26 +27,58 @@ $(document).ready(() => {
     // console.log(datepicker.selectedDates);
     // console.log($(".booking-card .date-dropdown__first-input .input-field__input").val());
 
-    let daysNumber = $(".booking-card__days");
+    let priceAndDaysExpression = $(".booking-card__price-and-days-expression");
+    let intermediateResult = $(".booking-card__intermediate-result");
+
+    let discountField = $(".booking-card__discount-size");
+    let discountSize = discountField.data("discount");
+
+    if(discountSize) {
+        discountField.html("Сбор за услуги: скидка " + priceToString(discountSize));
+    } else {
+        discountField.html("Сбор за услуги:");
+        discountSize = 0;
+    };
+
+    let additionalServicesCostField = $(".booking-card__additional-services-cost");
+    let additionalServicesCost = additionalServicesCostField.data("services");
+
+    if(additionalServicesCost) {
+        additionalServicesCostField.html(priceToString(additionalServicesCost));
+    } else {
+        additionalServicesCostField.html("0₽");
+        additionalServicesCost = 0;
+    };
+
 
     if(datepicker) {
         let stayLengthDays = (datepicker.selectedDates[1] - datepicker.selectedDates[0]) / (1000 * 3600 * 24);
         if(stayLengthDays == 1) {
-            daysNumber.html(stayLengthDays + " сутки");
+            priceAndDaysExpression.html(priceToString(roomPrice) + " x " + stayLengthDays + " сутки");
         } else if(stayLengthDays > 1) {
-            daysNumber.html(stayLengthDays + " суток");
+            priceAndDaysExpression.html(priceToString(roomPrice) + " x " + stayLengthDays + " суток");
         };
+        intermediateResult.html(priceToString(roomPrice * stayLengthDays));
+
+        let totalPriceField = $(".booking-card__total-price");
+        let totalPrice = roomPrice * stayLengthDays - discountSize + additionalServicesCost;
+        totalPriceField.html(priceToString(totalPrice));
 
         $(".booking-card .date-dropdown__first-input .input-field__input").bind('DOMSubtreeModified', function() {
             // console.log(1);
             if(datepicker.selectedDates[1] && datepicker.selectedDates[0]) {
                 stayLengthDays = (datepicker.selectedDates[1] - datepicker.selectedDates[0]) / (1000 * 3600 * 24);
                 if(stayLengthDays == 1) {
-                    daysNumber.html(stayLengthDays + " сутки");
+                    priceAndDaysExpression.html(priceToString(roomPrice) + " x " + stayLengthDays + " сутки");
                 } else if(stayLengthDays > 1) {
-                    daysNumber.html(stayLengthDays + " суток");
+                    priceAndDaysExpression.html(priceToString(roomPrice) + " x " + stayLengthDays + " суток");
                 };
             };
+
+            intermediateResult.html(priceToString(roomPrice * stayLengthDays));
+
+            totalPrice = roomPrice * stayLengthDays - discountSize + additionalServicesCost;
+            totalPriceField.html(priceToString(totalPrice));
         })
     };
 });
