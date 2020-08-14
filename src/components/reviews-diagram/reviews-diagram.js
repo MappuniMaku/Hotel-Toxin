c01be0009ec76967227d5ab7c794c8bb7f33f448
Grigory Telenok <telenok.grigory@gmail.com>
2020-08-15 01:12:31 +1000

    Finished reviews-diagram

--- src/components/reviews-diagram/reviews-diagram.js
+++ src/components/reviews-diagram/reviews-diagram.js
@@ -1,1 +1,27 @@
 import './reviews-diagram.scss';
+
+$(document).ready(() => {
+    // let reviewsNumber = $(".reviews-diagram__reviews-text").data("reviews");
+    let reviewsTextField = $(".reviews-diagram__reviews-text");
+    let reviewsNumber = reviewsTextField.data("reviews");
+    reviewsNumber = `${reviewsNumber}`;
+    let reviewsLastSymbol = reviewsNumber[reviewsNumber.length - 1];
+    // console.log(reviewsLastSymbol);
+    // console.log(reviewsNumber);
+
+    if(reviewsNumber == 0) {
+        reviewsTextField.html("голосов");
+    } else if(reviewsNumber == 1) {
+        reviewsTextField.html("голос");
+    } else if(reviewsNumber > 1 && reviewsNumber < 5) {
+        reviewsTextField.html("голоса");
+    } else if(reviewsNumber >= 5 && reviewsNumber <= 20) {
+        reviewsTextField.html("голосов");
+    } else if(reviewsNumber > 20 && reviewsLastSymbol == 1) {
+        reviewsTextField.html("голос");
+    } else if(reviewsNumber > 20 && (reviewsLastSymbol > 1 && reviewsLastSymbol < 5)) {
+        reviewsTextField.html("голоса");
+    } else {
+        reviewsTextField.html("голосов");
+    };
+});
